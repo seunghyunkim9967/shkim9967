46fd0275e45471af88a8e4dce5f034739569f2ed
seunghyunkim9967 <dnfheh@naver.com>
2022-04-14 19:54:06 +0900

    [backjoon]

---
+++ src/backSh/back10926.java
@@ -0,0 +1,14 @@
+package backSh;
+
+import java.util.Scanner;
+
+public class back10926 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scan = new Scanner(System.in);
+        String id = scan.next(); 
+        System.out.print(id + "??!");
+	}
+
+}
