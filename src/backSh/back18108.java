cb9b5743d9af4b0dc270d24e769f9457b4629c16
seunghyunkim9967 <dnfheh@naver.com>
2022-04-14 21:50:18 +0900

    [back1]

---
+++ src/backSh/back18108.java
@@ -0,0 +1,17 @@
+package backSh;
+
+import java.util.Scanner;
+
+public class back18108 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        int yyyy = scan.nextInt();
+        
+        yyyy = yyyy - 543;
+        
+        System.out.print(yyyy);
+        
+	}
+	
+}
---
+++ src/backSh/back2525.java
@@ -0,0 +1,28 @@
+package backSh;
+
+import java.util.Scanner;
+
+public class back2525 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+        int hour = scan.nextInt();
+        int minute = scan.nextInt();
+        int ovenmin = scan.nextInt();
+        
+        int time = hour * 60 + minute + ovenmin;
+        
+        hour = time / 60;
+        minute = time % 60;
+        
+        if (hour >= 24) {
+        	hour = hour - 24;
+        }
+        
+        
+        System.out.println(hour + " " + minute);
+        System.out.println(ovenmin);
+	}
+
+}
